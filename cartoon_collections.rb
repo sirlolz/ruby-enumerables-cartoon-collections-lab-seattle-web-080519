5e5802c99069bd909f501da3de73f882d035cee3
Jared Burnham <zlolris97@gmail.com>
2019-07-09 15:57:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index() do |array, index|
+    inde = index + 1
+    puts "#{inde}. #{array}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |array| 
+    "#{array.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|element| element.length > 4}
 end
 
 def find_the_cheese# code an argument here
